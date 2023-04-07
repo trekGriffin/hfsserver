2f6caea5a1688c85d03036ce8b980de07fd6e891
trek <xephidriver@gmail.com>
2023-04-07 21:09:58 +0800

    saturday

--- linux.ps1
+++ linux.ps1
@@ -1,5 +1,7 @@
+set productname hfsserver
 rm bin/*
 #go build -o bin/activation$(git describe --tags --dirty).exe -ldflags "-X main.appVersion=$(git describe --tags --dirty) -X 'main.appDate=$(date)' "  
 go env -w GOOS=linux
-go build -o bin/hfsserver$(git describe --tags --dirty) -ldflags "-X main.appVersion=$(git describe --tags --dirty) -X 'main.appDate=$(date)' "  
+go build -o bin/$productname$(git describe --tags --dirty) -ldflags "-X main.appVersion=$(git describe --tags --dirty) -X 'main.appDate=$(date)' "  
 go env -w GOOS=windows
+curl -T ./bin/$productname$(git describe --tags --dirty) http://10.10.10.3/upload/$productname
